6c69669cd8fd614b62baa0d98239a26b286e4be0
noseew <noseew@qq.com>
2020-08-10 22:15:53 +0800

    netty

--- network-programming-java/netty4-demo/src/test/java/org/song/network/nettydemo/demo/demo01/second/server/SecondServer.java
+++ network-programming-java/netty4-demo/src/test/java/org/song/network/nettydemo/demo/demo01/second/server/SecondServer.java
@@ -6,18 +6,30 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+/**
+ * 自定义一个简单的 netty实现的 TCP socket通信服务器
+ */
 public class SecondServer {
 
     public static void main(String[] args) throws InterruptedException {
         EventLoopGroup boss = new NioEventLoopGroup();
         EventLoopGroup worker = new NioEventLoopGroup();
 
-        ServerBootstrap bootstrap = new ServerBootstrap();
-        bootstrap.group(worker, worker)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(null);
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    // handler 是 boss线程组执行的
+//                    .handler()
+                    // childHandler 是 worker 线程组执行的
+                    .childHandler(new SecondServerInitializer());
 
-        ChannelFuture channelFuture = bootstrap.bind(8080).sync();
-        channelFuture.channel().closeFuture().sync();
+            ChannelFuture channelFuture = bootstrap.bind(8080).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
     }
 }
