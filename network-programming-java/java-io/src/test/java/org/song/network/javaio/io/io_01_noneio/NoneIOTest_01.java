209871d142fbff6562fad811bcde8876fb6e578e
noseew <noseew@qq.com>
2020-08-29 19:46:45 +0800

    IO

--- network-programming-java/java-io/src/test/java/org/song/network/javaio/io/io_01_noneio/NoneIOTest_01.java
+++ network-programming-java/java-io/src/test/java/org/song/network/javaio/io/io_01_noneio/NoneIOTest_01.java
@@ -1,17 +1,17 @@
 package org.song.network.javaio.io.io_01_noneio;
 
 import org.junit.jupiter.api.Test;
-import org.song.network.javaio.utils.IOUtils;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 /**
- * 对应 Linux网络编程 五种IO模型中的 NIO, 非阻塞IO
+ * 对应 Linux网络编程 五种IO模型中的 NIO, 非阻塞IO(注意: 不是多路复用IO)
+ *
+ * 利用ServerSocketChannel 的非阻塞特性, 循环向系统内核发起询问, 是否有事件到达, 如果有就处理
  */
 public class NoneIOTest_01 {
 
@@ -19,23 +19,16 @@ public class NoneIOTest_01 {
 
     @Test
     public void client() throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("127.0.0.1", port));
+        if (socketChannel.isConnected()) {
+            ByteBuffer outBuffer = ByteBuffer.wrap("客户端发送请求: hello".getBytes());
+            socketChannel.write(outBuffer);
 
-        Socket socket = new Socket("127.0.0.1", port);
-
-        PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-        writer.println("client : 我发消息了");
-        writer.flush();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String body;
-        while (true) {
-            body = reader.readLine();
-            if (body == null || "end".equals(body)) {
-                break;
-            }
-            System.out.println("client 收到服务端消息 :" + body);
+            outBuffer.clear();
+            socketChannel.read(outBuffer);
+            System.out.println("来自服务器的响应: " + new String(outBuffer.array()));
         }
-        IOUtils.close(writer, reader, socket);
     }
 
     @Test
@@ -52,7 +45,7 @@ public class NoneIOTest_01 {
             if (accept == null) {
                 continue;
             }
-//            System.out.println("连接成功！" + accept.getRemoteAddress());
+            System.out.println("连接成功！" + accept.getRemoteAddress());
             // 创建读取的缓冲区
             ByteBuffer buffer = ByteBuffer.allocate(1024);
             int read = accept.read(buffer);
@@ -61,7 +54,7 @@ public class NoneIOTest_01 {
                 String msg = new String(data).trim();
                 System.out.println("服务端收到信息事件: " + msg);
                 //回写数据
-                ByteBuffer outBuffer = ByteBuffer.wrap("server res ...".getBytes());
+                ByteBuffer outBuffer = ByteBuffer.wrap("服务端给客户端响应...".getBytes());
                 accept.write(outBuffer);// 将消息回送给客户端
             }
         }
--- network-programming-java/java-io/src/test/java/org/song/network/javaio/io/io_01_noneio/NoneIOTest_02.java
+++ network-programming-java/java-io/src/test/java/org/song/network/javaio/io/io_01_noneio/NoneIOTest_02.java
@@ -2,6 +2,7 @@ package org.song.network.javaio.io.io_01_noneio;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -12,7 +13,9 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 /**
- * 零拷贝发送 数据
+ * 对应 Linux网络编程 五种IO模型中的 NIO, 非阻塞IO(注意: 不是多路复用IO)
+ * <p>
+ * 客户端使用 零拷贝 向服务端发送 数据
  */
 public class NoneIOTest_02 {
 
@@ -29,8 +32,10 @@ public class NoneIOTest_02 {
         socketChannel.connect(new InetSocketAddress(port));
         socketChannel.configureBlocking(true);
 
-        FileChannel fileChannel = new FileInputStream("").getChannel();
+        FileChannel fileChannel = new FileInputStream(new File("Charset-readme.md")).getChannel();
         fileChannel.transferTo(0, fileChannel.size(), socketChannel);
+        System.out.println("客户端已发送完成数据");
+        // 注意: 要等服务端读完数据在断开
         fileChannel.close();
     }
 
@@ -43,6 +48,8 @@ public class NoneIOTest_02 {
         socket.setReuseAddress(true);
         socket.bind(new InetSocketAddress(port));
 
+        System.out.println("服务端启动成功");
+
         ByteBuffer allocate = ByteBuffer.allocate(1024);
         while (true) {
             /**
@@ -50,13 +57,19 @@ public class NoneIOTest_02 {
              * 如果是阻塞模式, 则会阻塞到有链接
              */
             SocketChannel accept = serverChannel.accept();
+            if (accept == null) {
+                continue;
+            }
+            System.out.println("连接成功: " + accept.getRemoteAddress().toString());
             int readCount = 0;
             while (readCount != -1) {
                 try {
                     readCount = accept.read(allocate);
                 } catch (Exception e) {
                     e.printStackTrace();
+                    break;
                 }
+                System.out.println(new String(allocate.array()));
                 // 倒带, 读满了 重复读
                 allocate.rewind();
             }
