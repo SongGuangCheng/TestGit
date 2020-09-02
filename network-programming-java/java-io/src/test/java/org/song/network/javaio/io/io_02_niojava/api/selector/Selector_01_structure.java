bb31125160679cdd473384f4acb25a5918a8a151
noseew <noseew@qq.com>
2020-09-02 21:33:17 +0800

    IO

---
+++ network-programming-java/java-io/src/test/java/org/song/network/javaio/io/io_02_niojava/api/selector/Selector_01_structure.java
@@ -0,0 +1,19 @@
+package org.song.network.javaio.io.io_02_niojava.api.selector;
+
+import org.junit.jupiter.api.Test;
+
+public class Selector_01_structure {
+
+    /**
+     * Selector 抽象类
+     * -    - AbstractSelector 抽象类
+     *
+     * SelectorProvider 抽象类
+     * -    - SelectorProviderImpl 类(未开源)
+     * -    -   - WindowsSelectorProvider windows 系统下的实现 类(未开源)
+     */
+    @Test
+    public void test_01_base() {
+
+    }
+}
